a41a847b01eacf1c55bdab672a3b9ede54408709
Kevin Burke <kevin@twilio.com>
2013-02-02 20:00:07 -0800

    add fabfile

---
+++ fabfile.py
@@ -0,0 +1,20 @@
+"""
+Script for deploying code from my local laptop to a remote server.
+
+Author: Kevin Burke <kev@inburke.com>
+
+Usage:
+
+    fab deploy
+"""
+from fabric.api import env, run, cd, local
+
+env.hosts = ['kevinburke@kevinburke.webfactional.com']
+env.user = 'kevinburke'
+env.key_filename = '/Users/kevin/.ssh/webfaction_rsa'
+
+def deploy():
+    local("git checkout master; git push origin master")
+    code_dir = '~/webapps/athenian'
+    with cd(code_dir):
+        run("git pull https://github.com/kevinburke/athenian.me.git")
